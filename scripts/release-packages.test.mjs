46e5f4e2a90ebbb4bc0b6db176820e200dec29d4
arjunbharti <arjunbharti774@gmail.com>
2026-08-26 17:51:21 +0530

    Derive release artifacts from package manifests

---
+++ scripts/release-packages.test.mjs
@@ -0,0 +1,43 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+
+import { releasePackages } from "./release-packages.mjs";
+
+test("release artifacts derive immutable filenames from package manifests", () => {
+  assert.deepEqual(
+    releasePackages.map(({ file, name, version }) => ({ file, name, version })),
+    [
+      {
+        file: "flagwire-schema-0.2.0.tgz",
+        name: "@flagwire/schema",
+        version: "0.2.0",
+      },
+      {
+        file: "flagwire-evaluate-0.2.0.tgz",
+        name: "@flagwire/evaluate",
+        version: "0.2.0",
+      },
+      {
+        file: "flagwire-sdk-js-0.2.1.tgz",
+        name: "@flagwire/sdk-js",
+        version: "0.2.1",
+      },
+      {
+        file: "flagwire-sdk-react-0.2.0.tgz",
+        name: "@flagwire/sdk-react",
+        version: "0.2.0",
+      },
+      {
+        file: "flagwire-sdk-node-0.2.0.tgz",
+        name: "@flagwire/sdk-node",
+        version: "0.2.0",
+      },
+      {
+        file: "flagwire-typegen-0.1.1.tgz",
+        name: "flagwire-typegen",
+        version: "0.1.1",
+      },
+    ],
+  );
+  assert.equal(new Set(releasePackages.map(({ file }) => file)).size, releasePackages.length);
+});
